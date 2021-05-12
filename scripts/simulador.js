3b0b63614c24067333911466da650618ab0dea7c
Gustavo Gomes <gustsgomesbr@hotmail.com>
2021-05-12 10:52:47 -0400

    parte lógica iniciada + select's corrigidos

---
+++ scripts/simulador.js
@@ -0,0 +1,108 @@
+var eloBoost = [
+  {
+    liga: "Bronze",
+    divisao: "IV",
+    valor: 15
+  },
+  {
+    liga: "Bronze",
+    divisao: "III",
+    valor: 15
+  },
+  {
+    liga: "Bronze",
+    divisao: "II",
+    valor: 15
+  },
+  {
+    liga: "Bronze",
+    divisao: "I",
+    valor: 15
+  },
+  {
+    liga: "Prata",
+    divisao: "IV",
+    valor: 15
+  },
+  {
+    liga: "Prata",
+    divisao: "III",
+    valor: 19
+  },
+  {
+    liga: "Prata",
+    divisao: "II",
+    valor: 19
+  },
+  {
+    liga: "Prata",
+    divisao: "I",
+    valor: 19
+  },
+  {
+    liga: "Ouro",
+    divisao: "IV",
+    valor: 19
+  },
+  {
+    liga: "Ouro",
+    divisao: "III",
+    valor: 23
+  },
+  {
+    liga: "Ouro",
+    divisao: "II",
+    valor: 23
+  },
+  {
+    liga: "Ouro",
+    divisao: "I",
+    valor: 23
+  },
+  {
+    liga: "Platina",
+    divisao: "IV",
+    valor: 30
+  },
+  {
+    liga: "Platina",
+    divisao: "III",
+    valor: 30
+  },
+  {
+    liga: "Platina",
+    divisao: "II",
+    valor: 30
+  },
+  {
+    liga: "Platina",
+    divisao: "I",
+    valor: 30
+  },
+  {
+    liga: "Diamante",
+    divisao: "IV",
+    valor: 30
+  },
+  {
+    liga: "Diamante",
+    divisao: "III",
+    valor: 60
+  },
+  {
+    liga: "Diamante",
+    divisao: "II",
+    valor: 75
+  },
+  {
+    liga: "Diamante",
+    divisao: "I",
+    valor: 90
+  },
+  {
+    liga: "Mestre",
+    divisao: "I",
+    valor: 110
+  },
+]
+
